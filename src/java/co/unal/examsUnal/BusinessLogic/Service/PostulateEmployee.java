616f247aef59acc14f6f1e2a4dbfa176bc419be7
Yeison Garcia <yedgarciago@gmail.com>
2016-05-02 23:09:10 -0500

    change in PostulateEmployee

--- src/java/co/unal/examsUnal/BusinessLogic/Service/PostulateEmployee.java
+++ src/java/co/unal/examsUnal/BusinessLogic/Service/PostulateEmployee.java
@@ -35,9 +35,9 @@ public class PostulateEmployee {
         PostulateEmployeeRequestDto.Employee myEmployee = parameter.getEmployee();
         int gender = myEmployee.getGender().compareTo(PostulateEmployeeRequestDto.Gender.MALE) == 0 ? 1 : 0; 
 
-        User myUser = myUserController.login(myEmployee.getDocument(), parameter.getPassword());
+        User myUser = myUserController.login(myEmployee.getDocument(), "password");
         if ( myUser == null )
-            myUser = myUserController.register(myEmployee.getDocument(), myEmployee.getFirstName()+" "+myEmployee.getLastName(), myEmployee.getEmail(), parameter.getPassword(), gender,"user");
+            myUser = myUserController.register(myEmployee.getDocument(), myEmployee.getFirstName()+" "+myEmployee.getLastName(), myEmployee.getEmail(), "password", gender,"user");
         ExamRegisterController myExamRC = new ExamRegisterController();
         ExamController myExamC = new ExamController();
         Exam myExam;
